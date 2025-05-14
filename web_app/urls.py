3d5b8a368d4840e645acd0ff1b2054252d4d3adb
HondamirKobilov <118079181+HondamirKobilov@users.noreply.github.com>
2025-05-14 20:58:13 +0500

    333

--- web_app/urls.py
+++ web_app/urls.py
@@ -1,7 +1,6 @@
-from django.contrib import admin
-from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('valyuta_converter.urls')),
@@ -9,7 +8,6 @@ urlpatterns = [
     path('kontent_download/', include('kontent_download.urls')),
 ]
 
-# Media fayllarga xizmat qilish
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)  # BU QATORNI QO‘SHING
