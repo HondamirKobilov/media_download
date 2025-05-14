57bec29cc50c6ad586150fe181ddc896b94aded9
HondamirKobilov <118079181+HondamirKobilov@users.noreply.github.com>
2025-05-14 21:18:00 +0500

    555

--- web_app/settings.py
+++ web_app/settings.py
@@ -116,11 +116,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
-    BASE_DIR / 'kontent_download/static',  # static fayllar papkasi
+    BASE_DIR / 'kontent_download/static/kontent_download',  # static fayllar papkasi
 ]
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
