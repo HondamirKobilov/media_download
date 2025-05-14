a7832ca8ea77b37297b94e48c2e949c2afb0bf5f
HondamirKobilov <118079181+HondamirKobilov@users.noreply.github.com>
2025-05-14 21:02:28 +0500

    444

--- web_app/urls.py
+++ web_app/urls.py
@@ -1,3 +1,5 @@
+from django.contrib import admin
+from django.urls import path, include  # MUHIM: path va include import qilinishi kerak
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -10,4 +12,4 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)  # BU QATORNI QO‘SHING
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
